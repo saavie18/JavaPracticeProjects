f085842c1b5289c3cf7a6a8f0a7fdebc3a4d88e3
Savitri Iyer <51967426+saavie18@users.noreply.github.com>
2019-10-15 17:22:13 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,56 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int total = 0;
+        char operator;
+        int leftnum;
+        int rightnum;
+        int result = 0;
+        System.out.println("Enter the expression for normal calculator");
+        String input = sc.next();
+        char[] inputArray = input.toCharArray();
+        for (int i = 1; i < inputArray.length; i+=2) {
+            //System.out.print(inputArray[i]);
+            //operator = inputArray[i];
+            if(i==1) {
+                leftnum = Character.getNumericValue(inputArray[i - 1]);
+            }
+            else
+            {
+                leftnum = result;
+            }
+            operator = inputArray[i];
+            rightnum = Character.getNumericValue(inputArray[i+1]);
+             result = execute(leftnum, operator, rightnum);
+        }
+        System.out.println("Total = " +result);
+    }
+   public static int execute (int leftnum , char operator , int rightnum)
+   {
+       int total;
+       switch (operator)
+       {
+               case '+' :
+                   total = leftnum + rightnum ;
+                   break;
+               case '-' :
+                   total = leftnum - rightnum ;
+                   break;
+               case '*' :
+                   total = leftnum * rightnum ;
+                   break;
+               case  '/' :
+                   total = leftnum != 0 ? leftnum / rightnum : 0 ;
+                   break;
+                   default:
+                   System.out.println("Does not match any opcode");
+                   total = 0;
+
+           }
+           return total;
+       }
+
+   }
 
