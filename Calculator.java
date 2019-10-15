5cb12adbf4b598089e5bd347728d06305220b222
Savitri Iyer <51967426+saavie18@users.noreply.github.com>
2019-10-15 17:57:57 +0530

    reviewed

--- Calculator.java
+++ Calculator.java
@@ -11,6 +11,21 @@ public class Calculator {
         System.out.println("Enter the expression for normal calculator");
         String input = sc.next();
         char[] inputArray = input.toCharArray();
+        
+        
+        /* try traversing the whole uinput array and form the expression 
+        Example : input : 1+2 . input : 1+2-1 . input : 1+2+6
+        
+        ##Logic 
+        Step 1 : travese the input from 0th index to last index of input string 
+        Step 2 : on every traversal compare if  inputArray[i] >= 0 || inputArray[i] <=9 then its a number and assign it to leftnum
+        Step 3 : on every traversal compare if  inputArray[i].equals to  + - * / then its a operator and assign it to operator
+        Step 4 : on every traversal compare if  iinputArray[i] >= 0 || inputArray[i] <=9 then its a number and assign it to rightnum
+        Step 5 : once you have these three parameter invoke execute method and assign its result to leftnum and follow the Steps 4 again 
+        Keep performing the while loop until iterator reaches to last index of input String 
+
+        */
+        
         for (int i = 1; i < inputArray.length; i+=2) {
             //System.out.print(inputArray[i]);
             //operator = inputArray[i];
