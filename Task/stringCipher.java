170e0c66b65208cc88b9768fbd1e19e5a817edf4
Savitri Iyer <51967426+saavie18@users.noreply.github.com>
2020-01-19 23:26:13 +0530

    Update stringCipher.java

--- Task/stringCipher.java
+++ Task/stringCipher.java
@@ -1,1 +1,46 @@
 
+package com.jetbrains.Task1.StringExamples;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+
+public class stringCipher {
+    final static int lower_case_a = 'a';
+    final static int uppercase_a = 'A';
+    final static int lower_case_z = 'z';
+    final static int upper_case_z = 'Z';
+
+
+    public static void encryptDecrypt(String st) throws InterruptedException{
+        //TimeUnit.SECONDS.sleep(2);
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0;i<st.length();i++)
+        {
+            char c = st.charAt(i);
+            if((int)c >= lower_case_a && (int)c <= lower_case_z)
+            {
+                //sb.append((char)(lower_case_z - c + lower_case_a));
+                sb.append((char)(lower_case_z - (c+32) + lower_case_a));
+            }
+            else if ((int)c >= uppercase_a && (int)c <= upper_case_z)
+            {
+                sb.append((char)(upper_case_z - (c-32) + uppercase_a));
+            }
+        }
+        System.out.println(sb.toString());
+
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        System.out.println("Enter a string");
+        String str = sc.next();
+        long lStartTime = System.currentTimeMillis();
+        encryptDecrypt(str);
+        long lEndTime = System.currentTimeMillis();
+        long output = lEndTime - lStartTime;
+        System.out.println("Elapsed time in milliseconds: " + output);
+
+    }
+}
